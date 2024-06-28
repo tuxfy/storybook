5775632e8e533123308110929796aa7521bea531
tuxfy <coder.tux@posteo.de>
2024-06-28 08:30:18 +0200

    moved page

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Button } from './components/Button'
 import { Header } from './components/Header'
 import { SearchableSelect } from './components/SearchableSelect'
-import { Page } from './components/Page'
+import { Page } from './pages/Page'
 
 export { Button, Header, SearchableSelect, Page }
