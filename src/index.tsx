bef553b897aeac542591bdce32814e029dfdb730
tuxfy <coder.tux@posteo.de>
2024-06-28 09:21:58 +0200

    changed component export

--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import { Button } from './components/Button'
-import { Header } from './components/Header'
-import { SearchableSelect } from './components/SearchableSelect'
-import { Page } from './pages/Page'
-
-export { Button, Header, SearchableSelect, Page }
