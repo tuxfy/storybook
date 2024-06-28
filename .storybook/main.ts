eeae3e87625586d81abfcaa2f144aa0e46aae0a9
tuxfy <coder.tux@posteo.de>
2024-06-28 08:30:10 +0200

    from webpack to vite

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,21 +1,17 @@
-import type { StorybookConfig } from '@storybook/react-webpack5'
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-	stories: ['../src/**/*.stories.tsx'],
-	addons: [
-		'@storybook/addon-webpack5-compiler-swc',
-		'@storybook/addon-onboarding',
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@chromatic-com/storybook',
-		'@storybook/addon-interactions',
-	],
-	framework: {
-		name: '@storybook/react-webpack5',
-		options: {},
-	},
-}
-export default config
-
-//https://prateeksurana.me/blog/react-component-library-using-storybook-7/
-//https://dev.to/andypotts/creating-a-reusable-component-library-with-react-storybook-and-webpack-2ple
+  stories: ['../src/**/*.stories.tsx'],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+};
+export default config;
